84ac341712ffb6e560559bd6c5d57f11a99c2728
dragonblade316 <dragonblade316@yahoo.com>
2023-12-15 12:29:49 -0700

    things

--- pkgs/beyond_all_reason/default.nix
+++ pkgs/beyond_all_reason/default.nix
@@ -8,19 +8,19 @@
 	nss_latest,
 	gtk3,
 	binutils,
-}: appimageTools.wraptype2 {
+}: appimageTools.wrapType2 {
 	name = "beyond_all_reason";
 
 	src = fetchurl {
 			url = "https://github.com/beyond-all-reason/BYAR-Chobby/releases/download/v1.2470.0/Beyond-All-Reason-1.2470.0.AppImage";
-			sha256 = "";
+			sha256 = "sha256-nWt/ONvlv72wvDGW2J4F+YY1tFRjhXw8viC53H2+sck=";
 	};
 
-	extraPackages = [
+	extraPkgs = pkgs: [
 		SDL2
 		openal
 		fuse
-		fusePackages.fuse2
+		fusePackages.fuse_2
 		nss_latest
 		gtk3
 		binutils
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}: {
 	cascadeur = pkgs.libsForQt5.callPackage ./cascadeur {};
 	grammarly-languageserver = pkgs.callPackage ./grammarly-languageserver {};
+	beyond_all_reason = pkgs.callPackage ./beyond_all_reason {};
 }
