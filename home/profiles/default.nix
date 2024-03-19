e382f05ecebb47a19dcc9e7c09da15120425e589
dragonblade316 <dragonblade316@yahoo.com>
2024-03-19 17:02:09 -0600

    stuff

--- home/profiles/default.nix
+++ home/profiles/default.nix
@@ -8,40 +8,23 @@
 with inputs;
 
 let 
+  sharedModules = [
+    ../.
+		../shell
+    ../editors/neovim
+		inputs.stylix.homeManagerModules.stylix
+		inputs.nix-index-database.hmModules.nix-index
+	];
 
-    imports = [
-    #  hyprland.nixosModules.${system}.hm
-    ];
-    sharedModules = [
-      ../.
-      ../shell
-      ../editors/neovim
-			#inputs.hyprland.homeManagerModules.default
-			#inputs.nix-colors.homeManagerModules.default
-			inputs.stylix.homeManagerModules.stylix
-			inputs.nix-index-database.hmModules.nix-index
-    ];
-
-    homeImports = {
-      dragonblade316 = [./rog] ++ sharedModules ++ imports;
-    };
-
-		inherit (inputs.nix-colors) colors;
-
-    mkHome = {conf}: (
-			hm.lib.homeManagerConfiguration {
-			inherit pkgs;
-			modules = [conf] ++ sharedModules ++ imports;
-		});
+  mkHome = {conf}: (
+		hm.lib.homeManagerConfiguration {
+		inherit pkgs;
+		modules = [conf] ++ sharedModules ++ imports;
+	});
 
 
 in {
-	# dragonblade316 = mkHome { conf = ./main; };
-  
-	#this is my main machine
 	"dragonblade316@dragonblade316" = mkHome { conf = ./main; };
 	"dragonblade316@thomeserver" = mkHome { conf = ./homeserver; };
-	
 	"dragonblade316@wsl" = mkHome { conf = ./wsl; };
-  #"dragonblade316@rog" = mkHome { conf = ./rog; };
 }
