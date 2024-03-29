12735010356a280492b5fa34c16b8d816653cb68
dragonblade316 <dragonblade316@yahoo.com>
2024-03-28 23:58:10 -0600

    adding framework

---
+++ home/profiles/framework/default.nix
@@ -0,0 +1,24 @@
+{
+    imports = [
+      ../../programs/git.nix
+      ../../programs/misc.nix
+      ../../themeing.nix
+      ../../wayland/hypr
+		  # ../../programs/eww
+			../../wayland/waybar.nix
+			../../terminals/kitty.nix
+			../../programs/mako.nix
+			../../editors/vscode
+			../../editors/intellij-idea
+			../../programs/art.nix
+			../../programs/comms.nix
+			../../programs/games.nix
+			../../programs/misc.nix
+			../../programs/fuzzel.nix
+			../../programs/dev.nix
+			../../programs/syncthing.nix
+			# ../../programs/rstreamdeck.nix
+			../../programs/remote.nix
+    ];
+	
+}
